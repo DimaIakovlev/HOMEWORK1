c6442386676166960ec9e315939af51e118d84ad
Yakovlev Dima <zif18@rambler.ru>
2022-10-14 22:21:08 +0500

    Zadacha004 complit

---
+++ Zadacha004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Вывод максимального числа среди введенных трех
+Console.Write ("Введите первое число: ");
+int MaxChislo = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int VtoroeChislo = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите третье число: ");
+int TretieChislo = Convert.ToInt32(Console.ReadLine());
+if (VtoroeChislo>MaxChislo)
+{
+MaxChislo = VtoroeChislo;
+}
+if (TretieChislo>MaxChislo)
+{
+    MaxChislo = TretieChislo;
+}
+Console.WriteLine("Максимальное число введенное Вами: " + MaxChislo);
