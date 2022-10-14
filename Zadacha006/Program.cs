81bd5d5fc063b82dbec35fba43e10868291ec356
Yakovlev Dima <zif18@rambler.ru>
2022-10-14 22:33:38 +0500

    Zadacha006 complit

---
+++ Zadacha006/Program.cs
@@ -0,0 +1,11 @@
+﻿// Определение четности числа
+Console.Write ("Введите число для проверки: ");
+int Chislo = Convert.ToInt32(Console.ReadLine());
+if (Chislo %2 == 0)
+{
+    Console.Write ("Число " + Chislo + " является четным");
+}
+else
+{
+    Console.Write ("Число " + Chislo + " является нечетным");
+}
