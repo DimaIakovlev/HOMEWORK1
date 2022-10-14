59c463848dc691481b12b2bf402707e3a06c8b2e
Yakovlev Dima <zif18@rambler.ru>
2022-10-14 22:00:52 +0500

    Zadacha002 complit

---
+++ Zadacha002/Program.cs
@@ -0,0 +1,13 @@
+﻿// Сравнение двух чисел и вывод большего и меньшего
+Console.Write ("Введите первое число: ");
+int PervoeChislo = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int VtoroeChislo = Convert.ToInt32(Console.ReadLine());
+if (PervoeChislo > VtoroeChislo) 
+{
+    Console.WriteLine(PervoeChislo + " > " + VtoroeChislo);
+}
+else 
+{
+    Console.WriteLine(PervoeChislo + " < " + VtoroeChislo);
+}
